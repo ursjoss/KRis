645c76823ebaa7c5369b9a5c44f6d596ae603b51
Urs Joss <u.joss@calltrade.ch>
2019-11-10 19:58:40 +0100

    Bump assertj-core from 3.13.2 to 3.14.0

--- subprojects/example_javaclient/example_javaclient.gradle.kts
+++ subprojects/example_javaclient/example_javaclient.gradle.kts
@@ -3,5 +3,5 @@ dependencies {
 
     implementation("io.reactivex.rxjava2:rxjava:2.2.14")
 
-    testImplementation("org.assertj:assertj-core:3.13.2")
+    testImplementation("org.assertj:assertj-core:3.14.0")
 }

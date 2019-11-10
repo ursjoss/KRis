fc8ad1bc70ec2262e29cbc78dfb772b2db4661fd
Urs Joss <u.joss@calltrade.ch>
2019-11-10 19:58:27 +0100

    Bump rxjava from 2.2.13 to 2.2.14

--- subprojects/example_javaclient/example_javaclient.gradle.kts
+++ subprojects/example_javaclient/example_javaclient.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     implementation(project(":kris"))
 
-    implementation("io.reactivex.rxjava2:rxjava:2.2.13")
+    implementation("io.reactivex.rxjava2:rxjava:2.2.14")
 
     testImplementation("org.assertj:assertj-core:3.13.2")
 }

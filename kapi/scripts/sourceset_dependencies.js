0a9982f08387147a3fc837748a7929a0d66e0fa5
Urs Joss <urs.joss@gmx.ch>
2022-10-09 14:01:32 +0200

    Publish guide for 0.4.0-rc.0.10+127929b

--- kapi/scripts/sourceset_dependencies.js
+++ kapi/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":guide:dokkaHtml/main": [], ":kris-core:dokkaHtml/main": [], ":kris-guide:dokkaHtml/main": [], ":kris-io:dokkaHtml/main": []}'
+sourceset_dependencies = '{":kris-io:dokkaHtmlPartial/main":[],":kris-core:dokkaHtmlPartial/main":[]}'

2d24e9753542638290452149137797c3aafad54d
Urs Joss <urs.joss@gmx.ch>
2022-10-10 23:35:06 +0200

    refactor: [#89] Remove spek dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,7 +97,7 @@ subprojects.forEach { subProject ->
 
         withType<Test> {
             useJUnitPlatform {
-                includeEngines("junit-jupiter", "spek2", "kotest")
+                includeEngines("junit-jupiter", "kotest")
             }
         }
         withType<DokkaTaskPartial>().configureEach {

0d53e56933b5780b41efecf5fd47b31f79ffe2d8
Urs Joss <urs.joss@gmx.ch>
2023-02-26 09:13:59 +0100

    refactor: [#129] Add (disabled) integration test

--- subprojects/kris-io/src/integrationTest/java/ch/difty/kris/KRisIOIntegrationTest.kt
+++ subprojects/kris-io/src/integrationTest/java/ch/difty/kris/KRisIOIntegrationTest.kt
@@ -4,17 +4,19 @@ import ch.difty.kris.domain.RisRecord
 import ch.difty.kris.domain.RisType
 import org.amshove.kluent.shouldContainAll
 import org.amshove.kluent.shouldHaveSize
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.io.File
 
 private const val FILE_PATH = "src/integrationTest/resources/sample.ris"
-private const val PAPER_COUNT = 3
+private const val PAPER_COUNT = 4
 
-@Suppress("FunctionName", "S100", "SpellCheckingInspection")
+@Suppress("FunctionName", "S100", "SpellCheckingInspection", "MaxLineLength")
 internal class KRisIOIntegrationTest {
 
     //region Importing
     @Test
+    @Disabled // TODO enable after implementation
     fun `can read from reader`() {
         val parsed = File(FILE_PATH).bufferedReader().process()
         parsed shouldHaveSize PAPER_COUNT
@@ -22,21 +24,25 @@ internal class KRisIOIntegrationTest {
         parsed.map { it.title } shouldContainAll setOf(
             "Exposure to traffic noise and air pollution and risk for febrile seizure: a cohort study.",
             "Impact of Road Traffic Pollution on Pre-eclampsia and Pregnancy-induced Hypertensive Disorders.",
-            "Exposure to long-term air pollution and road traffic noise in relation to cholesterol: A cross-sectional study."
+            "Exposure to long-term air pollution and road traffic noise in relation to cholesterol: A cross-sectional study.",
+            "∂ for Data: Differentiating Data Structures",
         )
     }
 
     @Test
+    @Disabled // TODO enable after implementation
     fun `can read from file`() {
         File(FILE_PATH).process() shouldHaveSize PAPER_COUNT
     }
 
     @Test
+    @Disabled // TODO enable after implementation
     fun `can read from file path`() {
         FILE_PATH.process() shouldHaveSize PAPER_COUNT
     }
 
     @Test
+    @Disabled // TODO enable after implementation
     fun `can read from file stream`() {
         File(FILE_PATH).inputStream().process() shouldHaveSize PAPER_COUNT
     }
@@ -48,7 +54,7 @@ internal class KRisIOIntegrationTest {
     private val records = listOf(
         RisRecord(type = RisType.JOUR, authors = mutableListOf("Author, A"), title = "Some title"),
         RisRecord(type = RisType.JOUR, authors = mutableListOf("Other-Author, B"), title = "Some other title"),
-        RisRecord(type = RisType.JOUR, authors = mutableListOf("Third-Author, C"), title = "Some other title")
+        RisRecord(type = RisType.JOUR, authors = mutableListOf("Third-Author, C"), title = "Some other title"),
     )
 
     @Test

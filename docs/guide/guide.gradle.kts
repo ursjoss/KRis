3d6324f9cbe47eb7b8b255e1609acc371cd694e9
Urs Joss <urs.joss@gmx.ch>
2024-12-11 15:31:39 +0100

    Back to https url

--- docs/guide/guide.gradle.kts
+++ docs/guide/guide.gradle.kts
@@ -64,7 +64,7 @@ afterEvaluate {
     val htmlJavadocOutputDir = dokkaOutputDir.resolve("javadocCollector")
     val gitPassword = providers.environmentVariable("GRGIT_USER")
     gitPublish {
-        repoUri.set("git@github.com:ursjoss/KRis.git")
+        repoUri.set("https://github.com/ursjoss/KRis.git")
         branch.set("gh-pages")
         contents {
             project.tasks.findByName("createGuide")?.outputs?.files?.let {

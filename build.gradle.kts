b9180c1500b13caf23c4800ec97d2017771defc8
Urs Joss <urs.joss@gmx.ch>
2025-09-28 12:56:55 +0200

    chore: use proper urls for OSSRH staging api

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ sonarqube {
 nexusPublishing {
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://central.sonatype.com/api/v1/publisher/"))
-            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/api/v1/publisher/snapshots/"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             val mcUsername = providers.environmentVariable("MC_USERNAME")
             val mcPassword = providers.environmentVariable("MC_PASSWORD")
             if (mcUsername.isPresent && mcPassword.isPresent) {
